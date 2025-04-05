a1ed1c048d3ea27ff7a4f557eb93874cf52fc80f
xiBread <oscar.leevermeren@gmail.com>
2025-04-05 09:55:42 -0400

    feat: add single-instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,7 +23,19 @@ pub struct AppState {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
         .plugin(
             tauri_plugin_sql::Builder::new()
                 .add_migrations("sqlite:emotes.db", migrations::emotes())
