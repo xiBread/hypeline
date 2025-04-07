cffa81d5af8f8081ab62ea5f909d0a3a55ad0de7
xiBread <oscar.leevermeren@gmail.com>
2025-04-06 23:56:23 -0400

    fix: move log in to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import type { AuthUser, FollowedChannel } from "$lib/twitch-api";
 	import ChannelList from "$lib/components/ChannelList.svelte";
 	import { invoke } from "@tauri-apps/api/core";
+	import { getAuthUrl } from "$lib/auth";
 
 	const { children, data } = $props();
 
@@ -40,10 +41,30 @@
 	});
 </script>
 
-<div class="flex">
-	<ChannelList {channels} />
+{#if settings.user}
+	<div class="flex">
+		<ChannelList {channels} />
 
-	<main class="grow">
-		{@render children()}
-	</main>
-</div>
+		<main class="grow">
+			{@render children()}
+		</main>
+	</div>
+{:else}
+	<a
+		class="bg-twitch m-auto flex items-center gap-2.5 rounded-md px-4 py-2 font-medium text-white"
+		href={getAuthUrl().toString()}
+	>
+		<svg
+			class="size-5 fill-white"
+			role="img"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"
+			/>
+		</svg>
+
+		Log in with Twitch
+	</a>
+{/if}
--- src/routes/[channel]/+page.svelte
+++ src/routes/[channel]/+page.svelte
@@ -1,7 +1,6 @@
 <!-- MOST CODE IN THIS FILE (and related) IS TEMPORARY DURING DEVELOPMENT; WILL CLEAN UP LATER -->
 <script lang="ts">
 	import WebSocket from "@tauri-apps/plugin-websocket";
-	import { getAuthUrl } from "$lib/auth";
 	import Chat, { type Message } from "$lib/components/Chat.svelte";
 	import Input from "$lib/components/Input.svelte";
 	import {
@@ -138,37 +137,17 @@
 </script>
 
 <div class="flex h-screen flex-col">
-	{#if settings.user}
-		<Chat class="h-full grow" {messages} />
+	<Chat class="h-full grow" {messages} />
 
-		<div class="border-t p-2">
-			<Input
-				class="focus-visible:ring-twitch focus-visible:border-twitch focus-visible:ring-1"
-				type="text"
-				placeholder="Send a message"
-				maxlength={500}
-				autocapitalize="off"
-				autocorrect="off"
-				onkeypress={send}
-			/>
-		</div>
-	{:else}
-		<a
-			class="bg-twitch m-auto flex items-center gap-2.5 rounded-md px-4 py-2 font-medium text-white"
-			href={getAuthUrl().toString()}
-		>
-			<svg
-				class="size-5 fill-white"
-				role="img"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"
-				/>
-			</svg>
-
-			Log in with Twitch
-		</a>
-	{/if}
+	<div class="border-t p-2">
+		<Input
+			class="focus-visible:ring-twitch focus-visible:border-twitch focus-visible:ring-1"
+			type="text"
+			placeholder="Send a message"
+			maxlength={500}
+			autocapitalize="off"
+			autocorrect="off"
+			onkeypress={send}
+		/>
+	</div>
 </div>
