4d392c915dafb810a522c7a0414a14920721d31a
xiBread <oscar.leevermeren@gmail.com>
2025-04-19 01:37:16 -0400

    refactor: make app state a class

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,23 +1,30 @@
 import type WebSocket from "@tauri-apps/plugin-websocket";
 import { RuneStore } from "@tauri-store/svelte";
+import { SvelteMap } from "svelte/reactivity";
 import type { AuthUser } from "./auth-user.svelte";
 import { Channel } from "./channel.svelte";
 import type { Emote } from "./channel.svelte";
 
-interface AppState {
-	loading: boolean;
-	ws?: WebSocket;
-	wsSessionId?: string;
-	user?: AuthUser;
-	active: Channel;
-	globalEmotes: Map<string, Emote>;
+class AppState {
+	#active = $state<Channel>();
+
+	public loading = $state(true);
+	public ws?: WebSocket;
+	public wsSessionId?: string;
+	public user?: AuthUser;
+
+	public readonly globalEmotes = new SvelteMap<string, Emote>();
+
+	public get active() {
+		return this.#active ?? Channel.empty();
+	}
+
+	public set active(channel: Channel) {
+		this.#active = channel;
+	}
 }
 
-export const app = $state<AppState>({
-	loading: true,
-	active: Channel.empty(),
-	globalEmotes: new Map(),
-});
+export const app = new AppState();
 
 // eslint-disable-next-line ts/consistent-type-definitions
 export type Settings = {
