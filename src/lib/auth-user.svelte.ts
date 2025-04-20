335fe10bbc05b5b9b5d46c5f3ac864adf7a40f12
xiBread <oscar.leevermeren@gmail.com>
2025-04-20 18:05:47 -0400

    refactor: load emotes in `EmotePicker`

--- src/lib/auth-user.svelte.ts
+++ src/lib/auth-user.svelte.ts
@@ -1,16 +1,8 @@
 import { invoke } from "@tauri-apps/api/core";
-import type { UserEmote, UserWithColor } from "./tauri";
+import type { UserWithColor } from "./tauri";
 import { User } from "./user";
 
-interface ChannelEmote {
-	username: string;
-	profile_picture_url: string;
-	emotes: Omit<UserEmote, "owner" | "owner_profile_picture_url">[];
-}
-
 export class AuthUser extends User {
-	public emotes: ChannelEmote[] = [];
-
 	public constructor(
 		data: UserWithColor,
 		public readonly token: string,
@@ -27,29 +19,5 @@ export class AuthUser extends User {
 
 		const user = new AuthUser(data, token);
 		return user;
-	}
-
-	public async loadEmotes() {
-		if (this.emotes.length) return this.emotes;
-
-		const grouped: Record<string, ChannelEmote> = {};
-
-		const emotes = await invoke<UserEmote[]>("get_user_emotes");
-		emotes.sort((a, b) => a.owner.localeCompare(b.owner));
-
-		for (const { owner, owner_profile_picture_url, ...emote } of emotes) {
-			if (!grouped[owner]) {
-				grouped[owner] = {
-					username: owner,
-					profile_picture_url: owner_profile_picture_url,
-					emotes: [],
-				};
-			}
-
-			grouped[owner].emotes.push(emote);
-		}
-
-		this.emotes = Object.values(grouped);
-		return this.emotes;
 	}
 }
--- src/lib/components/EmotePicker.svelte
+++ src/lib/components/EmotePicker.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { invoke } from "@tauri-apps/api/core";
 	import { Popover, Tabs } from "bits-ui";
-	import { app } from "$lib/state.svelte";
+	import { onMount } from "svelte";
+	import type { UserEmote } from "$lib/tauri";
 
 	interface Props {
 		open?: boolean;
@@ -8,7 +10,19 @@
 		anchor?: HTMLElement;
 	}
 
+	interface EmoteGroup {
+		displayName: string;
+		profilePictureUrl: string;
+		emotes: Omit<UserEmote, "owner" | "owner_profile_picture_url">[];
+	}
+
 	let { open = $bindable(false), input, anchor }: Props = $props();
+
+	let channels = $state<EmoteGroup[]>([]);
+
+	onMount(async () => {
+		channels = await fetchEmotes();
+	});
 
 	function appendEmote(name: string) {
 		if (!input) return;
@@ -19,65 +33,81 @@
 			input.value = name;
 		}
 	}
+
+	async function fetchEmotes() {
+		console.log("fetching emotes");
+
+		const grouped: Record<string, EmoteGroup> = {};
+
+		const emotes = await invoke<UserEmote[]>("get_user_emotes");
+		emotes.sort((a, b) => a.owner.localeCompare(b.owner));
+
+		for (const { owner, owner_profile_picture_url, ...emote } of emotes) {
+			if (!grouped[owner]) {
+				grouped[owner] = {
+					displayName: owner,
+					profilePictureUrl: owner_profile_picture_url,
+					emotes: [],
+				};
+			}
+
+			grouped[owner].emotes.push(emote);
+		}
+
+		return Object.values(grouped);
+	}
 </script>
 
-{#if app.user}
-	{#await app.user.loadEmotes() then groups}
-		<Popover.Root bind:open>
-			<Popover.Portal>
-				<Popover.Content
-					class="bg-muted overflow-hidden rounded border"
-					customAnchor={anchor}
-					side="top"
-					sideOffset={12}
-					collisionPadding={8}
+<Popover.Root bind:open>
+	<Popover.Portal>
+		<Popover.Content
+			class="bg-muted overflow-hidden rounded border"
+			customAnchor={anchor}
+			side="top"
+			sideOffset={12}
+			collisionPadding={8}
+		>
+			<Tabs.Root class="flex" orientation="vertical">
+				<Tabs.List
+					class="bg-sidebar flex max-h-96 flex-col gap-3 overflow-y-auto border-r p-2"
 				>
-					<Tabs.Root class="flex" orientation="vertical">
-						<Tabs.List
-							class="bg-sidebar flex max-h-96 flex-col gap-3 overflow-y-auto border-r p-2"
-						>
-							{#each groups as channel}
-								<Tabs.Trigger value={channel.username}>
-									<img
-										class="size-8 rounded-full"
-										src={channel.profile_picture_url}
-										alt={channel.username}
-									/>
-								</Tabs.Trigger>
-							{/each}
-						</Tabs.List>
+					{#each channels as channel}
+						<Tabs.Trigger value={channel.displayName}>
+							<img
+								class="size-8 rounded-full"
+								src={channel.profilePictureUrl}
+								alt={channel.displayName}
+							/>
+						</Tabs.Trigger>
+					{/each}
+				</Tabs.List>
 
-						{#each groups as channel}
-							<Tabs.Content
-								class="max-h-96 overflow-y-auto"
-								value={channel.username}
-							>
-								<div class="bg-sidebar border-b p-2">
-									{channel.username}'s emotes
-								</div>
+				{#each channels as channel}
+					<Tabs.Content
+						class="max-h-96 overflow-y-auto"
+						value={channel.displayName}
+					>
+						<div class="bg-sidebar border-b p-2">
+							{channel.displayName}'s emotes
+						</div>
 
-								<div
-									class="grid grid-cols-7 content-start gap-2 p-2"
+						<div class="grid grid-cols-7 content-start gap-2 p-2">
+							{#each channel.emotes as emote}
+								<button
+									title={emote.name}
+									onclick={() => appendEmote(emote.name)}
 								>
-									{#each channel.emotes as emote}
-										<button
-											title={emote.name}
-											onclick={() =>
-												appendEmote(emote.name)}
-										>
-											<img
-												class="size-8"
-												src="https://static-cdn.jtvnw.net/emoticons/v2/{emote.id}/{emote.format}/dark/3.0"
-												alt={emote.name}
-											/>
-										</button>
-									{/each}
-								</div>
-							</Tabs.Content>
-						{/each}
-					</Tabs.Root>
-				</Popover.Content>
-			</Popover.Portal>
-		</Popover.Root>
-	{/await}
-{/if}
+									<img
+										class="size-8"
+										src="https://static-cdn.jtvnw.net/emoticons/v2/{emote.id}/{emote.format}/dark/3.0"
+										alt={emote.name}
+									/>
+								</button>
+							{/each}
+						</div>
+					</Tabs.Content>
+				{/each}
+			</Tabs.Root>
+		</Popover.Content>
+	</Popover.Portal>
+</Popover.Root>
