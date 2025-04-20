c4d0ca6ec7a7c0775fd9da6e14ed6a1484bc8b1e
xiBread <oscar.leevermeren@gmail.com>
2025-04-20 16:22:18 -0400

    refactor: use interface for `Settings`
    removes the need to ignore lint; not really important other than for consistency

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,11 +1,12 @@
 import { RuneStore } from "@tauri-store/svelte";
 
-// eslint-disable-next-line ts/consistent-type-definitions
-export type Settings = {
+export interface Settings {
+	// Index signature needed for RuneStore
+	[key: string]: unknown;
 	user: { id: string; token: string } | null;
 	lastJoined: string | null;
 	timeFormat: "auto" | "12" | "24";
-};
+}
 
 export const settings = new RuneStore<Settings>("settings", {
 	user: null,
