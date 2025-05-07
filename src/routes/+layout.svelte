b3db1ca39cef4481c5ef00f6cf1a5a29f7ea7673
xiBread <oscar.leevermeren@gmail.com>
2025-05-07 15:54:29 -0400

    fix: connect in root page
    layout mount doesn't rerun so in the event of logging in, connections aren't being made and errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,23 +2,10 @@
 	import "../app.css";
 	import { Tooltip } from "bits-ui";
 	import { ModeWatcher } from "mode-watcher";
-	import { onMount } from "svelte";
-	import { goto } from "$app/navigation";
 	import Sidebar from "$lib/components/Sidebar.svelte";
 	import { settings } from "$lib/settings";
-	import { connect } from "$lib/twitch";
 
 	const { children } = $props();
-
-	onMount(async () => {
-		if (settings.state.user) {
-			await connect();
-		}
-
-		if (settings.state.user && settings.state.lastJoined) {
-			await goto(`/${settings.state.lastJoined}`);
-		}
-	});
 </script>
 
 <ModeWatcher />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
 	import { settings } from "$lib/settings";
+	import { connect } from "$lib/twitch";
+
+	onMount(async () => {
+		if (!settings.state.user) return;
+
+		await connect();
+
+		if (settings.state.lastJoined) {
+			await goto(`/${settings.state.lastJoined}`);
+		}
+	});
 </script>
 
-{#if settings.state.user}
-	<div class="flex size-full items-center justify-center">
-		<p class="text-center">
-			Select a channel from your following list to start chatting
-		</p>
-	</div>
-{/if}
+<div class="flex size-full items-center justify-center">
+	<p class="text-center">
+		Select a channel from your following list to start chatting
+	</p>
+</div>
