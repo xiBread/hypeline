b67d86bd1f47d073e9fb983ae9b671e5e02dd74f
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-05-22 19:15:21 -0400

    fix: only process current user for subscription end events

--- src/lib/handlers/eventsub/channel-subscription-end.ts
+++ src/lib/handlers/eventsub/channel-subscription-end.ts
@@ -1,9 +1,12 @@
 import { SystemMessage } from "$lib/message";
+import { app } from "$lib/state.svelte";
 import { defineHandler } from "../helper";
 
 export default defineHandler({
 	name: "channel.subscription.end",
 	handle(data, channel) {
+		if (data.user_id !== app.user?.id) return;
+
 		const tier = data.tier === "Prime" ? "Prime" : `Tier ${data.tier[0]}`;
 		const text = `Your ${data.is_gift ? "gifted" : ""} ${tier} subscription has ended.`;
 
