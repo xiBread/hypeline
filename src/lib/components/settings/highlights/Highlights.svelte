452fcab172e5eeedd78e2aa8c73b3b1015c20ea7
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-05-24 22:45:57 -0400

    add global enable

--- src/lib/components/settings/highlights/Highlights.svelte
+++ src/lib/components/settings/highlights/Highlights.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+	import { settings } from "$lib/settings";
+	import Switch from "../../ui/Switch.svelte";
 	import Custom from "./Custom.svelte";
 	import Viewers from "./Viewers.svelte";
 </script>
 
 <div class="space-y-6">
-	<hgroup>
-		<h1 class="mb-2">Highlights</h1>
+	<h1>Highlights</h1>
 
-		<p class="text-muted-foreground mr-8 text-sm">
+	<Switch bind:checked={settings.state.highlights.enabled}>
+		<span class="font-medium">Highlight messages</span>
+
+		{#snippet description()}
 			Message highlights allow you to easily identify different types of viewers or when
 			specific keywords are sent in chat.
-		</p>
-	</hgroup>
+		{/snippet}
+	</Switch>
 
 	<div class="divide-border divide-y *:py-6 *:first:pt-0 *:last:pb-0">
 		<Viewers />
