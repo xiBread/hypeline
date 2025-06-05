2b8302ce538bf28679ef7196262f972b0577fbef
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-06-05 03:49:40 -0400

    fix: add devtools json plugin
    wasn't causing any issues but it gets rid of annoying 404 error when opening chrome devtools

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 // @ts-expect-error - Node.js global
 // eslint-disable-next-line node/prefer-global/process
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [devtoolsJson(), tailwindcss(), sveltekit()],
 	clearScreen: false,
 	server: {
 		port: 1420,
