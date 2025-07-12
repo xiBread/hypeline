58c805f07e52e160c6ace48fa29bb747ea28190b
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-07-12 08:37:54 -0400

    feat(chat): emote and user autocomplete (#52)

---
+++ src/lib/completer.svelte.ts
@@ -0,0 +1,134 @@
+import type { Suggestion } from "./components/Suggestions.svelte";
+import type { Emote } from "./tauri";
+import type { User } from "./user.svelte";
+
+interface CompleterSource {
+	emotes: Emote[];
+	viewers: User[];
+}
+
+interface SearchOptions<T> {
+	source: T[];
+	comparee: (item: T) => string;
+	map: (item: T) => Suggestion;
+}
+
+export class Completer {
+	#emoteOptions: SearchOptions<Emote>;
+	#viewerOptions: SearchOptions<User>;
+
+	public query = "";
+	public prefixed = false;
+
+	public current = $state(0);
+	public suggestions = $state<Suggestion[]>([]);
+
+	public constructor(
+		public readonly input: HTMLInputElement,
+		source: CompleterSource,
+	) {
+		this.#emoteOptions = {
+			source: source.emotes,
+			comparee: (item) => item.name,
+			map: (item) => ({
+				type: "emote" as const,
+				value: item.name,
+				display: item.name,
+				imageUrl: item.srcset[1].split(" ")[0],
+			}),
+		};
+
+		this.#viewerOptions = {
+			source: source.viewers,
+			comparee: (item) => item.username,
+			map: (item) => ({
+				type: "user" as const,
+				value: item.username,
+				display: item.displayName,
+				style: item.style,
+			}),
+		};
+	}
+
+	public complete(reset = true) {
+		const suggestion = this.suggestions[this.current];
+		let end = this.input.value.lastIndexOf(this.query);
+
+		if (this.query.startsWith("@")) {
+			end++;
+		}
+
+		const left = this.input.value.slice(0, end);
+		const right = this.input.value.slice(end + this.query.length);
+
+		this.input.value = `${left + suggestion.display} ${right.trim()}`;
+		this.input.focus();
+
+		const endPos = end + suggestion.display.length + 1;
+		this.input.setSelectionRange(endPos, endPos);
+
+		if (reset) {
+			this.reset();
+		} else {
+			this.query = suggestion.display;
+		}
+	}
+
+	public search(tab = false) {
+		const text = this.input.value;
+		const cursor = this.input.selectionStart ?? text.length;
+
+		const left = text.slice(0, cursor);
+		const lastWord = left.split(" ").pop();
+
+		if (!lastWord) {
+			this.suggestions = [];
+			return;
+		}
+
+		this.query = lastWord;
+
+		if (this.query.startsWith("@")) {
+			this.prefixed = true;
+			this.suggestions = this.#search(this.#viewerOptions);
+		} else if (this.query.startsWith(":")) {
+			this.prefixed = true;
+			this.suggestions = this.#search(this.#emoteOptions);
+		} else if (tab) {
+			this.suggestions = [
+				...this.#search(this.#emoteOptions, true),
+				...this.#search(this.#viewerOptions, true),
+			];
+		}
+	}
+
+	public next() {
+		this.current = (this.current + 1) % this.suggestions.length;
+	}
+
+	public prev() {
+		this.current = (this.current - 1 + this.suggestions.length) % this.suggestions.length;
+	}
+
+	public reset() {
+		this.query = "";
+		this.prefixed = false;
+		this.suggestions = [];
+		this.current = 0;
+	}
+
+	#search<T>(options: SearchOptions<T>, tab = false) {
+		const searchFunction = tab ? "startsWith" : "includes";
+		const query = tab ? this.query : this.query.slice(1);
+
+		if (!query) return [];
+
+		return options.source
+			.filter((item) =>
+				options.comparee(item).toLowerCase()[searchFunction](query.toLowerCase()),
+			)
+			.slice(0, 25)
+			.map(options.map)
+			.sort((a, b) => a.value.localeCompare(b.value));
+	}
+}
---
+++ src/lib/components/Suggestions.svelte
@@ -0,0 +1,86 @@
+<script lang="ts" module>
+	interface BaseSuggestion {
+		value: string;
+		display: string;
+	}
+
+	interface EmoteSuggestion extends BaseSuggestion {
+		type: "emote";
+		imageUrl: string;
+	}
+
+	interface UserSuggestion extends BaseSuggestion {
+		type: "user";
+		style: string;
+	}
+
+	export type Suggestion = EmoteSuggestion | UserSuggestion;
+</script>
+
+<script lang="ts">
+	import { Combobox } from "bits-ui";
+
+	interface Props {
+		anchor: HTMLElement | null;
+		open: boolean;
+		index: number;
+		suggestions: Suggestion[];
+		onselect: (suggestion: Suggestion) => void;
+	}
+
+	let { anchor, open = $bindable(), index, suggestions, onselect }: Props = $props();
+
+	const items = $state<(HTMLDivElement | null)[]>(Array.from({ length: 25 }, () => null));
+
+	$effect(() => {
+		items[index]?.scrollIntoView({ block: "nearest" });
+	});
+</script>
+
+<Combobox.Root
+	type="single"
+	onValueChange={(value) => {
+		const suggestion = suggestions.find((s) => s.value === value);
+		if (suggestion) onselect(suggestion);
+	}}
+	bind:open
+>
+	<Combobox.Portal>
+		<Combobox.Content
+			class="bg-card z-50 max-h-72 w-(--bits-combobox-anchor-width) overflow-y-auto rounded-md border p-1 shadow-lg"
+			customAnchor={anchor}
+			side="top"
+			sideOffset={8}
+		>
+			{#each suggestions as suggestion, i (suggestion.value)}
+				<Combobox.Item
+					class={[
+						"flex w-full items-center gap-2 rounded-sm px-2 py-1.5 text-left text-sm outline-none hover:cursor-pointer",
+						"data-current:bg-accent data-current:text-accent-foreground",
+					]}
+					title={suggestion.display}
+					value={suggestion.value}
+					data-current={index === i ? true : null}
+					onmouseenter={() => (index = i)}
+					bind:ref={items[i]}
+				>
+					{#if suggestion.type === "emote"}
+						<img
+							class="size-8 object-contain"
+							src={suggestion.imageUrl}
+							alt={suggestion.display}
+						/>
+
+						<span class="overflow-x-hidden overflow-ellipsis">
+							{suggestion.display}
+						</span>
+					{:else}
+						<span class="font-semibold" style={suggestion.style}>
+							{suggestion.display}
+						</span>
+					{/if}
+				</Combobox.Item>
+			{/each}
+		</Combobox.Content>
+	</Combobox.Portal>
+</Combobox.Root>
