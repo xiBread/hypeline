d0ca86566afabc62981c555edb08afad7e3285bd
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-07-16 10:29:10 -0400

    refactor(chat): update sub icons

--- src/lib/components/message/Sub.svelte
+++ src/lib/components/message/Sub.svelte
@@ -15,12 +15,34 @@
 
 <div class="bg-muted/50 my-0.5 border-l-4 p-2" style:border-color={app.joined?.user.color}>
 	<div class="flex gap-1">
-		<span
-			class={[
-				"iconify mt-px size-4 shrink-0",
-				sub.type === "sub_or_resub" ? "lucide--star" : "lucide--gift",
-			]}
-		></span>
+		{#if sub.type === "sub_or_resub"}
+			{#if sub.sub_plan === "Prime"}
+				<svg class="fill-current" width="20" height="20" viewBox="0 0 20 20">
+					<g>
+						<path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M18 5v8a2 2 0 0 1-2 2H4a2.002 2.002 0 0 1-2-2V5l4 3 4-4 4 4 4-3z"
+						/>
+					</g>
+				</svg>
+			{:else}
+				<svg
+					class="fill-current stroke-current stroke-2"
+					width="16"
+					height="16"
+					viewBox="0 0 24 24"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path
+						d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"
+					/>
+				</svg>
+			{/if}
+		{:else}
+			<span class="iconify lucide--gift mt-px size-4 shrink-0"></span>
+		{/if}
 
 		{#if sub.type === "sub_or_resub"}
 			<div class="flex flex-col gap-0.5">
