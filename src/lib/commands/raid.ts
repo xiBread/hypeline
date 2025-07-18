bbaade428019a5b31da4f699cf6e0bccf38cd8af
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-07-18 18:00:20 -0400

    (un)raid

--- src/lib/commands/raid.ts
+++ src/lib/commands/raid.ts
@@ -1,4 +1,5 @@
-import { defineCommand } from "./util";
+import { invoke } from "@tauri-apps/api/core";
+import { defineCommand, getTarget } from "./util";
 
 export default defineCommand({
 	name: "raid",
@@ -9,5 +10,25 @@ export default defineCommand({
 			required: true,
 		},
 	],
-	async exec(args, channel, user) {},
+	async exec(args, channel) {
+		const target = await getTarget(args[0], channel);
+		if (!target) return;
+
+		if (channel.user.id === target.id) {
+			channel.error = "The broadcaster cannot raid themselves.";
+			return;
+		}
+
+		try {
+			await invoke("raid", { fromId: channel.user.id, toId: target.id });
+		} catch (error) {
+			if (typeof error !== "string") return;
+
+			if (error.includes("cannot be") || error.includes("channel's settings")) {
+				channel.error = `${target.displayName} cannot be raided.`;
+			} else {
+				channel.error = "An unknown error occurred while trying to raid.";
+			}
+		}
+	},
 });
