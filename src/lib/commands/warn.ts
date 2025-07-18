d3b8cfd68725e2d8dc54fd6fdeb10c2af7edd598
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-07-18 12:41:50 -0400

    warn

--- src/lib/commands/warn.ts
+++ src/lib/commands/warn.ts
@@ -1,4 +1,5 @@
-import { defineCommand } from "./util";
+import { invoke } from "@tauri-apps/api/core";
+import { defineCommand, getTarget } from "./util";
 
 export default defineCommand({
 	name: "warn",
@@ -8,5 +9,31 @@ export default defineCommand({
 		{ name: "username", required: true },
 		{ name: "reason", required: true },
 	],
-	async exec(args, channel, user) {},
+	async exec(args, channel) {
+		const target = await getTarget(args[0], channel);
+		if (!target) return;
+
+		const reason = args.slice(1).join(" ");
+
+		if (!reason) {
+			channel.error = "Missing reason argument.";
+			return;
+		}
+
+		try {
+			await invoke("warn", {
+				broadcasterId: channel.user.id,
+				userId: target.id,
+				reason,
+			});
+		} catch (error) {
+			if (typeof error !== "string") return;
+
+			if (error.includes("may not be warned")) {
+				channel.error = `${target.displayName} may not be warned.`;
+			} else {
+				channel.error = "An unknown error occurred while trying to ban.";
+			}
+		}
+	},
 });
