64a5a8869dbd39a5e8ce10fb161ec228a70e667f
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-07-21 13:22:36 -0400

    (un)vip

--- src/lib/commands/unvip.ts
+++ src/lib/commands/unvip.ts
@@ -1,4 +1,5 @@
-import { defineCommand } from "./util";
+import { invoke } from "@tauri-apps/api/core";
+import { defineCommand, getTarget } from "./util";
 
 export default defineCommand({
 	name: "unvip",
@@ -10,5 +11,23 @@ export default defineCommand({
 			required: true,
 		},
 	],
-	async exec(args, channel, user) {},
+	async exec(args, channel) {
+		const target = await getTarget(args[0], channel);
+		if (!target) return;
+
+		try {
+			await invoke("remove_vip", {
+				broadcasterId: channel.user.id,
+				userId: target.id,
+			});
+		} catch (error) {
+			if (typeof error !== "string") return;
+
+			if (error.includes("is not")) {
+				channel.error = `${target.displayName} is not a VIP.`;
+			} else {
+				channel.error = "An unknown error occurred while trying to remove VIP.";
+			}
+		}
+	},
 });
