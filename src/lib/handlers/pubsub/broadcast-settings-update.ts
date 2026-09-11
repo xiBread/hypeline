dd1be85bdab710535fa3342b6790bc2ed0bcc4d8
Oliver Rose <hello@olivermrose.com>
2026-09-11 17:21:49 -0400

    update

--- src/lib/handlers/pubsub/broadcast-settings-update.ts
+++ src/lib/handlers/pubsub/broadcast-settings-update.ts
@@ -6,9 +6,11 @@ export default defineHandler({
 	name: "broadcast-settings-update",
 	handle(data) {
 		const channel = app.channels.get(data.channel_id);
+		if (!channel) return;
 
-		if (channel?.stream) {
+		if (channel.stream && data.old_status !== data.status) {
 			channel.stream.title = data.status;
+			channel.chat.notice(`The stream title has been updated to "${data.status}"`);
 		}
 	},
 });
